d63e84a55f8956d65c75c9c70f22da546ce1fbee
Daniel Leining <daniel@leining.org>
2018-06-15 13:22:36 -0400

    fix the mistakes of my past

--- getlasttrack/get_info.go
+++ getlasttrack/get_info.go
@@ -2,18 +2,87 @@ package getlasttrack
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strconv"
 )
 
 const (
 	api_base_url = "https://ws.audioscrobbler.com/2.0/"
-	debug = false
 )
+
+type RecentTracks struct {
+	Recenttracks struct {
+		Track []struct {
+			Artist struct {
+				Text string `json:"#text"`
+				Mbid string `json:"mbid"`
+			} `json:"artist"`
+			Name       string `json:"name"`
+			Streamable string `json:"streamable"`
+			Mbid       string `json:"mbid"`
+			Album      struct {
+				Text string `json:"#text"`
+				Mbid string `json:"mbid"`
+			} `json:"album"`
+			URL   string `json:"url"`
+			Image []struct {
+				Text string `json:"#text"`
+				Size string `json:"size"`
+			} `json:"image"`
+			Attr struct {
+				Nowplaying string `json:"nowplaying"`
+			} `json:"@attr,omitempty"`
+			Date struct {
+				Uts  string `json:"uts"`
+				Text string `json:"#text"`
+			} `json:"date,omitempty"`
+		} `json:"track"`
+		Attr struct {
+			User       string `json:"user"`
+			Page       string `json:"page"`
+			PerPage    string `json:"perPage"`
+			TotalPages string `json:"totalPages"`
+			Total      string `json:"total"`
+		} `json:"@attr"`
+	} `json:"recenttracks"`
+}
+
+type TrackInfo struct {
+	Track struct {
+		Name       string `json:"name"`
+		URL        string `json:"url"`
+		Duration   string `json:"duration"`
+		Streamable struct {
+			Text      string `json:"#text"`
+			Fulltrack string `json:"fulltrack"`
+		} `json:"streamable"`
+		Listeners string `json:"listeners"`
+		Playcount string `json:"playcount"`
+		Artist    struct {
+			Name string `json:"name"`
+			Mbid string `json:"mbid"`
+			URL  string `json:"url"`
+		} `json:"artist"`
+		Album struct {
+			Artist string `json:"artist"`
+			Title  string `json:"title"`
+			URL    string `json:"url"`
+			Image  []struct {
+				Text string `json:"#text"`
+				Size string `json:"size"`
+			} `json:"image"`
+		} `json:"album"`
+		Userplaycount string `json:"userplaycount"`
+		Userloved     string `json:"userloved"`
+		Toptags       struct {
+			Tag []struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"tag"`
+		} `json:"toptags"`
+	} `json:"track"`
+}
 
 type Track struct {
 	Album            string
@@ -22,126 +91,44 @@ type Track struct {
 	PlayCount        int
 	Tags             []string
 	CurrentlyPlaying bool
-	SpotifyUrl       string
 }
 
-/* GetTrack returns a Track. One or all fields may be nil, because last.fm rocks
-   This function will eventually be split off into it's own library.
-*/
 func GetTrack(user string, ApiKey string) (Track, error) {
-	var tname string
-	var aname string
-	var resp *http.Response
-	var err error
-	rt := Track{}
-	for i := 1; i < 3; i++ {
-		if debug {
-			fmt.Println("HTTP GET 1 TRY ", i)
-		}
-		resp, err = http.Get(fmt.Sprintf("%s?method=user.getrecenttracks&user=%s&api_key=%s&format=json&limit=1", api_base_url, user, ApiKey))
-		if err == nil {
-			break
-		}
-	}
+	resp, err := http.Get(fmt.Sprintf("%s?method=user.getrecenttracks&user=%s&api_key=%s&format=json&limit=1", api_base_url, user, ApiKey))
 	if err != nil {
-		if debug {
-			fmt.Println("Http Get 1:", err.Error())
-		}
-		return rt, err
+		return Track{}, err
 	}
-	if debug {
-		fmt.Println(resp)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
+	recentTrack := &RecentTracks{}
+	err = json.NewDecoder(resp.Body).Decode(recentTrack)
 	if err != nil {
-		if debug {
-			fmt.Println("Read resp.Body:", err.Error())
-		}
-		return rt, err
+		return Track{}, err
 	}
-	if debug {
-		fmt.Println(string(body))
-	}
-	var data map[string]interface{}
-	err = json.Unmarshal(body, &data)
+	t := recentTrack.Recenttracks.Track[0]
+	resp, err = http.Get(fmt.Sprintf("%s?method=track.getInfo&api_key=%s&artist=%s&track=%s&username=%s&format=json", api_base_url, ApiKey, t.Artist.Text, t.Name, user))
 	if err != nil {
-		if debug {
-			fmt.Println(err.Error())
-		}
-		return rt, err
+		return Track{}, err
 	}
-	var ct map[string]interface{}
-	if _, ok := data["recenttracks"].(map[string]interface{})["track"].([]interface{}); ok {
-		ct = data["recenttracks"].(map[string]interface{})["track"].([]interface{})[0].(map[string]interface{})
-	} else if _, ok := data["recenttracks"].(map[string]interface{})["track"].(map[string]interface{}); ok {
-		ct = data["recenttracks"].(map[string]interface{})["track"].(map[string]interface{})
-	} else {
-		return Track{}, errors.New("something broke")
-	}
-	aname = url.QueryEscape(ct["artist"].(map[string]interface{})["#text"].(string))
-	tname = url.QueryEscape(ct["name"].(string))
-	if cc, ok := ct["@attr"].(map[string]interface{}); ok {
-		if aa, bb := cc["nowplaying"].(string); bb && aa == "true" {
-			rt.CurrentlyPlaying = true
-			if debug {
-				fmt.Println("Now playing = true")
-			}
-		} else {
-			rt.CurrentlyPlaying = false
-			fmt.Println("Now playing = false")
-		}
-	}
-
-	for i := 1; i < 3; i++ {
-		if debug {
-			fmt.Println("HTTP GET 2 TRY ", i)
-		}
-		resp, err = http.Get(fmt.Sprintf("%s?method=track.getInfo&api_key=%s&artist=%s&track=%s&format=json&username=%s&autocorrect=1", api_base_url, ApiKey, aname, tname, user))
-		if err == nil {
-			break
-		}
-	}
+	ti := &TrackInfo{}
+	err = json.NewDecoder(resp.Body).Decode(ti)
 	if err != nil {
-		if debug {
-			fmt.Println("Error in Get 2:", err.Error())
-		}
-		return rt, err
+		return Track{}, err
 	}
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		if debug {
-			fmt.Println("Error in Read 2:", err.Error())
-		}
-		return rt, err
+	var tags []string
+	for _, x := range ti.Track.Toptags.Tag {
+		tags = append(tags, x.Name)
 	}
-	if debug {
-		fmt.Println(string(body))
+	playing := false
+	if t.Attr.Nowplaying == "true" {
+		playing = true
 	}
-	var data2 map[string]interface{}
-	err = json.Unmarshal(body, &data2)
-	if err != nil {
-		if debug {
-			fmt.Println(err.Error())
-		}
-		return rt, err
+	playcount, err := strconv.Atoi(ti.Track.Userplaycount)
+	rt := Track{
+		Album:            t.Album.Text,
+		Artist:           t.Artist.Text,
+		Name:             t.Name,
+		Tags:             tags,
+		CurrentlyPlaying: playing,
+		PlayCount:        playcount,
 	}
-	if al, kk := data2["track"].(map[string]interface{})["album"].(map[string]interface{}); kk {
-		rt.Album = al["title"].(string)
-	}
-	rt.Artist = data2["track"].(map[string]interface{})["artist"].(map[string]interface{})["name"].(string)
-	rt.Name = data2["track"].(map[string]interface{})["name"].(string)
-	if _, ta := data2["track"].(map[string]interface{})["userplaycount"].(string); ta {
-		rt.PlayCount, _ = strconv.Atoi(data2["track"].(map[string]interface{})["userplaycount"].(string))
-	}
-	if tags, lol := data2["track"].(map[string]interface{})["toptags"].(map[string]interface{}); lol {
-		if t, lol2 := tags["tag"].([]interface{}); lol2 {
-			for _, j := range t {
-				rt.Tags = append(rt.Tags, j.(map[string]interface{})["name"].(string))
-			}
-		} else {
-			rt.Tags = append(rt.Tags, tags["tag"].(map[string]interface{})["name"].(string))
-		}
-	}
-	rt.SpotifyUrl = GetSpotifyUrl(rt)
 	return rt, nil
 }
--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/blasphemy/urel"
-	"github.com/spf13/viper"
-	"github.com/blasphemy/np/getlasttrack"
 	"os"
 	"strings"
 	"text/template"
-)
 
-var (
-	debug bool
+	"github.com/blasphemy/np/getlasttrack"
+	"github.com/spf13/viper"
 )
 
 func RemoveSpaces(in string) string {
@@ -22,23 +18,14 @@ func RemoveSpaces(in string) string {
 func main() {
 	f := template.FuncMap{
 		"RemoveSpaces": RemoveSpaces,
-		"short":        urel.Short,
 	}
-	viper.AddConfigPath("$HOME/np")
-	viper.AddConfigPath("$HOME/.np")
-	viper.AddConfigPath("$HOME")
-	wd, err := os.Getwd()
-	if err != nil {
-		viper.AddConfigPath(wd)
-	}
+	viper.AddConfigPath(".")
 	viper.SetConfigName("np")
-	viper.SetDefault("template", "{{if .CurrentlyPlaying}}is now playing{{else}}last played{{end}} {{if .Artist}}[{{.Artist}}] - {{end}}{{if .Name}}[{{.Name}}] {{end}}{{if .Album}}On [{{.Album}}] {{end}}{{if .PlayCount}}[{{.PlayCount}} plays] {{end}}{{if .Tags}}[{{range $index, $element := .Tags}}{{if $index}} {{end}}#{{RemoveSpaces $element}}{{end}}]{{end}}{{if .SpotifyUrl}} [{{short .SpotifyUrl}}]{{end}}")
+	viper.SetDefault("template", "{{if .CurrentlyPlaying}}is now playing{{else}}last played{{end}} {{if .Artist}}[{{.Artist}}] - {{end}}{{if .Name}}[{{.Name}}] {{end}}{{if .Album}}On [{{.Album}}] {{end}}{{if .PlayCount}}[{{.PlayCount}} plays] {{end}}{{if .Tags}}[{{range $index, $element := .Tags}}{{if $index}} {{end}}#{{RemoveSpaces $element}}{{end}}]{{end}}")
 	viper.SetDefault("debug", false)
-	viper.SetDefault("username", "zxo0oxz")
 	viper.ReadInConfig()
 	apikey := viper.GetString("apikey")
 	username := viper.GetString("username")
-	debug = viper.GetBool("debug")
 	if apikey == "" {
 		fmt.Println("no api key")
 		os.Exit(1)
@@ -46,9 +33,6 @@ func main() {
 	tmpl := template.Must(template.New("format").Funcs(f).Parse(viper.GetString("template")))
 	track, err := getlasttrack.GetTrack(username, apikey)
 	if err != nil {
-		if debug {
-			fmt.Println(err.Error())
-		}
 		os.Exit(1)
 	}
 	err = tmpl.Execute(os.Stdout, track)
